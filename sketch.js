a66f4924594037b0b073c3cc2848fdaa2acf68fb
yuqi19990923 <sshuku221031@shukutoku.ed.jp>
2026-06-17 09:17:32 +0000

    いろいろやりました

--- sketch.js
+++ sketch.js
@@ -1,17 +1,23 @@
-function setup() {
-    createCanvas(400, 400);
-}
+let x;
+// let y;
 
+function setup() {
+   createCanvas(1800, 1000);
+    x=0
+    y=0
+    }
 function draw() {
     background(220);
-
-let x=200;
-let y=200;  
-let diameter=200;
-
-
-circle(width/2,height/2,50);
-}
+    x=x+20;
+    if(x>width){
+       x=0;
+    }
 
 
+    //  y=y+1;
+    circle(x,y,50);
+    textSize(200,200);
+    textAlign(CENTER,CENTER);
+    text("川﨑瑞生",width/2,height/2);
 
+    }
